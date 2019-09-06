c6b74e02a298b56064d89ab011e5d22ee14cca5b
TGA88 <supawut.thomas@gmail.com>
2019-09-06 22:18:44 +0700

    done responsive header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
+import './header.scss';
 
+import logo from 'app/asset/d_pic/Logo - Homes for All Birds.svg';
+import m_logo from 'app/asset/d_pic/HOMES FOR ALL BIRDS Symbol Logo.svg'
 export default class Header extends Component {
     render() {
         return (
             <div className={`sc-header`}>
-                <div className={`box`}>
-                    <img className={`logo`} src="" alt="header-logo"></img>
+               <img className={`logo`} src={logo} alt="header-logo"></img>
+               <img className={`mobile-logo`} src={m_logo} alt="header-logo"></img>
                     <div className={`nav`}>
                         <a href="#song1" className={`navlink`}>Discover More</a>
                         <a href="#home1" className={`navlink`}>See Your Home</a>
                     </div>
-                </div>
             </div>
         )
     }
